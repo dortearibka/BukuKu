e80d2bba56e98018688a340ea1d1cd9517fcab12
Dortea Ribka Tadete <153445261+dortearibka@users.noreply.github.com>
2024-06-05 02:23:48 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,234 @@
+<?php
+
+include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>tentang</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<div class="heading">
+   <h3>tentang kami</h3>
+   <p> <a href="home.php">Beranda</a> / Tentang Kami </p>
+</div>
+
+<section class="about">
+
+   <div class="flex">
+
+      <div class="image">
+         <img src="images/about.jpg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>Kenapa pilih kami?</h3>
+         <p>Toko buku tepercaya, temukan semua buku yang Anda sukai di sini.</p>
+         <p>Harga murah dan berkualitas, Belanja Sekarang !</p>
+         <a href="contact.php" class="btn">Hubungi Kami</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="reviews">
+
+   <h1 class="title">Ulasan Pelanggan</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/review.jpg" alt="">
+         <p>Toko buku yang sangat direkomendasikan.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>john deo</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/review.jpg" alt="">
+         <p>Pelayanan yang bagus.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>john deo</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/review.jpg" alt="">
+         <p>Mantap.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>john deo</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/review.jpg" alt="">
+         <p>Bagus</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>John deo</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/review.jpg" alt="">
+         <p>Keren</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>John deo</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/review.jpg" alt="">
+         <p>Sangat bagus</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>john deo</h3>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="authors">
+
+   <h1 class="title">penulis yang hebat</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/author.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>john deo</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/author.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>john deo</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/author.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>john deo</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/author.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>john deo</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/author.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>john deo</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/author.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>john deo</h3>
+      </div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>tautan cepat</h3>
+         <a href="home.php">Beranda</a>
+         <a href="about.php">Tentang Kami</a>
+         <a href="shop.php">Belanja</a>
+         <a href="contact.php">Kontak</a>
+      </div>
+
+      <div class="box">
+         <h3>tautan tambahan</h3>
+         <a href="login.php">Masuk</a>
+         <a href="register.php">Daftar</a>
+         <a href="cart.php">Keranjang</a>
+         <a href="orders.php">Pesanan</a>
+      </div>
+
+      <div class="box">
+         <h3>Informasi Kontak</h3>
+         <p> <i class="fas fa-phone"></i> +62 85176547801 </p>
+         <p> <i class="fas fa-phone"></i> +62 82289765789 </p>
+         <p> <i class="fas fa-envelope"></i> BukuKu@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Manado, North Sulawesi - 25000  </p>
+      </div>
+
+      <div class="box">
+         <h3>Ikuti Kami</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>BukuKu</span> </p>
+
+</section>
